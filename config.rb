36e84af2ad5e6c7b48236201449ea94600d3c36f
Claudio Vallejo <CVP@claudio.local>
2016-11-22 16:20:00 -0600

    Cloning Materiales Vallejo website. Initializing English project.

---
+++ config.rb
@@ -0,0 +1,43 @@
+###
+# General
+###
+
+# Reload the browser automatically whenever files change
+configure :development do
+  activate :livereload
+end
+
+# Pages with no layout
+page '/*.xml', layout: false
+page '/*.json', layout: false
+page '/*.txt', layout: false
+
+# Activate directory indexes
+activate :directory_indexes
+
+###
+# Proxies
+###
+
+# Dynamic Jobs Index + Detail
+#
+$jobs = 0
+data.jobs.list.each do |job|
+  if job.is_published
+    $jobs += 1
+  end
+end
+
+data.jobs.list.each do |job|
+  if $jobs > 0
+    if job.is_published
+      proxy "/empleos/#{ I18n.transliterate(job.title).downcase.strip.gsub(' ', '-') }/index.html", "/empleos/detail.html", :locals => { :job => job }, :ignore => true
+      proxy "/empleos/#{ I18n.transliterate(job.title).downcase.strip.gsub(' ', '-') }/aplicacion/index.html", "/empleos/application.html", :locals => { :job => job }, :ignore => true
+    end
+  else
+    ignore "/empleos/index.html"
+    ignore "/empleos/application.html"
+    ignore "/empleos/detail.html"
+    ignore "/aplicacion-enviada.html"
+  end
+end
---
+++ gulpfile.js
@@ -0,0 +1,57 @@
+var gulp = require('gulp'),
+    htmlmin = require('gulp-htmlmin'),
+    concat = require('gulp-concat'),
+    cssmin = require('gulp-csso'),
+    uglify = require('gulp-uglify'),
+    del = require('del'),
+    gzip = require('gulp-gzip'),
+    imagemin = require('gulp-imagemin'),
+    sitemap = require('gulp-sitemap');
+
+//  Minify .html
+gulp.task('markup', function(){
+  gulp.src('build/**/*.html')
+  .pipe(htmlmin())
+  .pipe(gulp.dest('build'))
+})
+
+//  Concatenate site.css + animate.css -> site.css
+//  Minify site.css + Gzip site.css
+gulp.task('styles', function(){
+  gulp.src(['build/stylesheets/site.css', 'build/stylesheets/animate.css'])
+  .pipe(concat('site.css'))
+  .pipe(cssmin())
+  .pipe(gulp.dest('build/stylesheets'))
+  .pipe(gzip())
+  .pipe(gulp.dest('build/stylesheets'))
+});
+
+//  Minify javascripts
+gulp.task('scripts', function(){
+  gulp.src(['build/javascripts/all.js', 'build/javascripts/dynamic.js'])
+  .pipe(uglify())
+  .pipe(gulp.dest('build/javascripts'))
+  .pipe(gzip())
+  .pipe(gulp.dest('build/javascripts'))
+});
+
+//  Optimize images
+gulp.task('images', function(){
+  gulp.src('build/images/**/*')
+    .pipe(imagemin({ progressive: true }))
+    .pipe(gulp.dest('build/images'))
+});
+
+//  Generate sitemap
+gulp.task('sitemap', function() {
+  gulp.src('build/**/*.html', {
+    read: false
+  })
+  .pipe(sitemap({
+    siteUrl: 'https://materialesvallejo.com'
+  }))
+  .pipe(gulp.dest('./build'));
+});
+
+// Run previously declared tasks on `gulp`
+gulp.task('build', ['markup', 'styles', 'scripts', 'images', 'sitemap']);
